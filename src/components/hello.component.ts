a362c919f7f1bbd72849abeca14c648fd21a2d6e
Esther White <esthersoftwaredev@gmail.com>
2025-06-08 11:15:01 +0300

    needed for icons

--- src/components/hello.component.ts
+++ src/components/hello.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input, signal } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-hello',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatIconModule],
   template: `
     <p>Hello from Angular!!</p>
 
@@ -15,8 +16,10 @@ import { MatButtonModule } from '@angular/material/button';
      }
 
     <button mat-raised-button (click)="toggle()">Toggle</button>
+    <mat-icon aria-hidden="false" aria-label="Example home icon" fontIcon="home"></mat-icon>
+
   `,
-  styleUrl: '../styles/angular-material.scss',
+  styleUrl: '../styles/global.scss',
 })
 export class HelloComponent {
   // @Input() helpText!: string;
