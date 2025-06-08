5f1658e7630fab820ebd868f8f20feeefc605dee
Esther White <esthersoftwaredev@gmail.com>
2025-06-08 09:40:22 +0300

    for shoelace icons - vite-plugin-static-copy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [
+      viteStaticCopy({
+        targets: [
+          {
+            src: "node_modules/@shoelace-style/shoelace/dist/assets",
+            dest: "shoelace-assets", // This puts all assets (including icons) to /shoelace-assets/assets/
+          },
+        ],
+      }),
+    ],
+  },
+});
